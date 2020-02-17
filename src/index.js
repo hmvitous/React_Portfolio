fe1974f02322284a7d1bfd25ce487bb818f9c354
Hunter <hmvitous@yahoo.com>
2020-02-17 14:27:24 +0100

    Fixes setup issues. Webpack installation primarily

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const HelloWorld = () => {
+    return <h2>Hello World</h2>
+
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"))
