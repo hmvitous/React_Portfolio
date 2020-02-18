7ff5de473e91ed0f24b1037bce40d10d9a0acdb0
Hunter <hmvitous@yahoo.com>
2020-02-18 10:42:44 +0100

    Adds a cypress test. Not passing. Creates the Projects.jsx and the About.jsx

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const About = () => {
+    return (
+        <div className="ui main container">
+            <h1 id="about-header" className="ui header">"About Me"</h1>
+            <p>
+                This is the section where I would type 
+                an about me paragraph
+            </p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Projects = () => {
+    return (
+        <div className="ui main container">
+            <h1 id="projects-header" className="ui header">My Projects</h1>
+            <p>
+                A place to type
+            </p>
+        </div>
+    )
+}
+
+export default Projects
